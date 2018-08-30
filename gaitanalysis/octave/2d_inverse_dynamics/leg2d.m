19906db4ce30919fd614ba65255d0d90b5e05563
Jason K. Moore <moorepants@gmail.com>
2018-08-29 22:36:41 -0700

    Use Octave interp1 instead of deprecated interp1q.

--- gaitanalysis/octave/2d_inverse_dynamics/leg2d.m
+++ gaitanalysis/octave/2d_inverse_dynamics/leg2d.m
@@ -91,9 +91,9 @@ function [angles, velocities, moments, forces] = leg2d(times, mocapdata, fpdata,
 		maxmissing = max(diff(validsamples))-1;			% determine largest gap
 		fprintf('Marker %d: %d samples are missing, longest gap is %d samples.\n', i, missing, maxmissing);
 		[xf, xd, xdd] = myfiltfilt(validtimes, d(validsamples,:), options.freq);
-		mocap_f(:,columns) = interp1q(validtimes, xf, times);		% resample filtered signal to original time stamps
-		mocap_d(:,columns) = interp1q(validtimes, xd, times);		% resample first derivative to original time stamps
-		mocap_dd(:,columns) = interp1q(validtimes, xdd, times);		% resample second derivative to original time stamps
+		mocap_f(:,columns) = interp1(validtimes, xf, times);		% resample filtered signal to original time stamps
+		mocap_d(:,columns) = interp1(validtimes, xd, times);		% resample first derivative to original time stamps
+		mocap_dd(:,columns) = interp1(validtimes, xdd, times);		% resample second derivative to original time stamps
 	end
 
 	% do the low-pass filtering on the force plate data
