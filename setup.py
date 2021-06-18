518ef7eeb1d5c6cd2347c2a41011b077ea07ebcb
Jason K. Moore <moorepants@gmail.com>
2021-06-18 20:37:22 +0200

    Add notes on support py 3 in documentation.

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(name='GaitAnalysisToolKit',
                    'Intended Audience :: Science/Research',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.7',
                    'Topic :: Scientific/Engineering :: Physics',
                   ],
       )
